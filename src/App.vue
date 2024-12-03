e77c3b86511965c8b2aa9db20e46fff62900f2af
RakshithaYS123 <rakshithays986@gmail.com>
2024-12-03 08:40:45 +0530

    03DEC20240840

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <HeroSection />
+    <ServicesSection />
+    <ContactSection />
+    <WhatsApp />
+    <ContactUs />
+  </div>
+</template>
+
+<script>
+import HeroSection from "./components/HeroSection.vue";
+import ServicesSection from "./components/ServicesSection.vue";
+import ContactSection from "./components/ContactSection.vue";
+import WhatsApp from "./components/WhatsApp.vue"
+import ContactUs from "./components/ContactUs.vue"
+
+export default {
+  name: "App",
+  components: {
+    HeroSection,
+    ServicesSection,
+    ContactSection,
+    WhatsApp,
+    ContactUs,
+  },
+};
+</script>
+
+<style>
+/* Global styling can go here if needed */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, sans-serif;
+  color: #333;
+  line-height: 1.6;
+}
+</style>
+
